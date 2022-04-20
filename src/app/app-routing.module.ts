9e0246ca69712227d74cafa036192034c5aa6384
ketijakrista <ketijakrista@gmail.com>
2022-04-20 21:14:16 +0300

    feat: add planners, header and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PlannerComponent } from './components/planner/planner.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'planner',
+    pathMatch: 'full'
+  },
+  {
+    path: 'planner',
+    component: PlannerComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlannerSimpleComponent } from './components/planner/planner-simple/planner-simple.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { PlannerComponent } from './components/planner/planner.component';
+import { PlannerWithCheckboxesComponent } from './components/planner/planner-with-checkboxes/planner-with-checkboxes.component';
+import { PlannerWithCheckboxesAndFilterComponent } from './components/planner/planner-with-checkboxes-and-filter/planner-with-checkboxes-and-filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlannerSimpleComponent,
+    HeaderComponent,
+    PlannerComponent,
+    PlannerWithCheckboxesComponent,
+    PlannerWithCheckboxesAndFilterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
