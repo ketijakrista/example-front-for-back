3705479c0ef49560fb152e1c5d53afed419a283a
ketijakrista <ketijakrista@gmail.com>
2022-04-24 13:34:06 +0300

    feat: add character page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlannerComponent } from './components/planner/planner.component';
 import { FormsComponent } from './components/forms/forms.component';
+import { CharactersComponent } from './components/characters/characters.component';
+import { CharacterComponent } from './components/characters/character/character.component';
 
 const routes: Routes = [
   {
@@ -16,6 +18,14 @@ const routes: Routes = [
   {
     path: 'forms',
     component: FormsComponent
+  },
+  {
+    path: 'characters',
+    component: CharactersComponent
+  },
+  {
+    path: 'characters/:id',
+    component: CharacterComponent
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { PlannerWithCheckboxesComponent } from './components/planner/planner-wit
 import { PlannerWithCheckboxesAndFilterComponent } from './components/planner/planner-with-checkboxes-and-filter/planner-with-checkboxes-and-filter.component';
 import { FormsComponent } from './components/forms/forms.component';
 import { LoginFormComponent } from './components/forms/login-form/login-form.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CharactersComponent } from './components/characters/characters.component';
+import { CharacterCardComponent } from './components/characters/character-card/character-card.component';
+import { CharacterComponent } from './components/characters/character/character.component';
 
 @NgModule({
   declarations: [
@@ -21,13 +25,17 @@ import { LoginFormComponent } from './components/forms/login-form/login-form.com
     PlannerWithCheckboxesComponent,
     PlannerWithCheckboxesAndFilterComponent,
     FormsComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    CharactersComponent,
+    CharacterCardComponent,
+    CharacterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
