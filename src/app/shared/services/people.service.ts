e3c187e77905bb1e2d6109d255574b0f2f884c5a
ketijakrista <ketijakrista@gmail.com>
2022-04-27 22:19:42 +0300

    feat: add star wars table

--- src/app/shared/services/people.service.ts
+++ src/app/shared/services/people.service.ts
@@ -1,20 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-import { BehaviorSubject, finalize, Observable, take, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { PeopleApiResponse } from '../models/people.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeopleService {
-  loading$ = new BehaviorSubject<boolean>(false);
-
   constructor(private http: HttpClient) {
-  }
-
-  getLoading(): Observable<boolean> {
-    return this.loading$.asObservable();
   }
 
   getPeople(page: number = 1): Observable<PeopleApiResponse> {
